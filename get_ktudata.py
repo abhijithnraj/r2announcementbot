2b62104c27b18fbc9ca5ad2ed88a7fc19c25c546
abhijithnraj <abhijithnrajg@gmail.com>
2017-07-20 18:10:58 +0530

    completed data acquisition without class

--- get_ktudata.py
+++ get_ktudata.py
@@ -9,27 +9,39 @@ class announcements:
         
 
 count=0
-l=[]
+l_title=[]
+l_desc=[]
+l_data=[]
 while(True):
     line=f.readline()
     if '<!-- <a target="_blank">' in line:
-        l.append(line)
-        if count>=5:
+        l_data.append(line)
+        if count>=9:
             break
         count+=1
+        
+def desc(p):
+   return p[p.find('</b>')+5:p.find('<!-- </a> -->')]
 
-
-
-def stripper(a):
+def title(a):
 	return a[a.find('<b>')+3:a.find('</b>')]
 
 
-list=[]
-for i in range(len(l)):
-	list.append(stripper(l[i]))
+for i in range(len(l_data)):
+	l_title.append(title(l_data[i]))
 
+
+
+for i in range(len(l_data)):
+    l_desc.append(desc(l_data[i]))
 
 import pickle
-f=open("ktudata.bin","wb")
-pickle.dump(list,f)
+f1=open("ktudata_title.bin","wb")
+pickle.dump(l_title,f1)
+f2=open("ktudata_desc.bin","wb")
+pickle.dump(l_desc,f2)
+
+f1.close()
+f2.close()
+
 
